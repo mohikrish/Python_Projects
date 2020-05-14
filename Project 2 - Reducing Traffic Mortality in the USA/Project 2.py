3b69695f0706d88b7776f4d308ae7f1757fabb41
MOHAN KRISHNA CHADRAM <64443941+mohikrish@users.noreply.github.com>
2020-05-14 18:50:52 -0400

    commit 1

--- Project 2 - Reducing Traffic Mortality in the USA/Project 2.py
+++ Project 2 - Reducing Traffic Mortality in the USA/Project 2.py
@@ -1,1 +1,16 @@
-print("Hello")
+# Import the `pandas` module as "pd"
+import pandas as pd
+
+# Read in `road-accidents.csv`
+car_acc = pd.read_csv('road-accidents.csv', comment='#', sep='|')
+
+# Save the number of rows columns as a tuple
+rows_and_cols = car_acc.shape
+print('There are {} rows and {} columns.\n'.format(rows_and_cols[0], rows_and_cols[1]))
+
+# Generate an overview of the DataFrame
+car_acc_information = car_acc.info()
+print(car_acc_information)
+
+# Display the last five rows of the DataFrame
+car_acc.tail()
